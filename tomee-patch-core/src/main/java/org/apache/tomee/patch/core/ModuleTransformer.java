f5a01815f0c39786b37c85c6f09590a50c13e154
David Blevins <david.blevins@gmail.com>
2020-06-12 20:41:15 -0700

    Fix module references

--- tomee-patch-core/src/main/java/org/apache/tomee/patch/core/ModuleTransformer.java
+++ tomee-patch-core/src/main/java/org/apache/tomee/patch/core/ModuleTransformer.java
@@ -23,4 +23,30 @@ public class ModuleTransformer extends ModuleVisitor {
     public ModuleTransformer(final int api, final ModuleVisitor moduleVisitor) {
         super(api, moduleVisitor);
     }
+
+    @Override
+    public void visitExport(String packaze, final int access, final String... modules) {
+        packaze = new Replace(packaze)
+                .replace("javax/xml/bind", "jakarta/xml/bind")
+                .get();
+        super.visitExport(packaze, access, modules);
+    }
+
+    @Override
+    public void visitProvide(String service, final String... providers) {
+        service = new Replace(service)
+                .replace("javax/xml/bind", "jakarta/xml/bind")
+                .replace("javax/xml/soap", "jakarta/xml/soap")
+                .get();
+        super.visitProvide(service, providers);
+    }
+
+    @Override
+    public void visitUse(String service) {
+        service = new Replace(service)
+                .replace("javax/xml/bind", "jakarta/xml/bind")
+                .replace("javax/xml/soap", "jakarta/xml/soap")
+                .get();
+        super.visitUse(service);
+    }
 }
